f7e44f1e8f22f434debd6aa3c0d6e49cb67080c0
Victor Martin <victormartin@centralite.com>
2017-11-06 16:45:29 -0600

    Measure key preeses timing

---
+++ WKeyTiming/KeyTimingForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WKeyTiming
+{
+    partial class KeyTimingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                _globalKeyboardHook?.Dispose();
+
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Status_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Status_textBox
+            // 
+            this.Status_textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Status_textBox.Location = new System.Drawing.Point(0, 0);
+            this.Status_textBox.Multiline = true;
+            this.Status_textBox.Name = "Status_textBox";
+            this.Status_textBox.ReadOnly = true;
+            this.Status_textBox.Size = new System.Drawing.Size(630, 228);
+            this.Status_textBox.TabIndex = 0;
+            this.Status_textBox.WordWrap = false;
+            // 
+            // KeyTimingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 228);
+            this.Controls.Add(this.Status_textBox);
+            this.Name = "KeyTimingForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.KeyTimingForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Status_textBox;
+    }
+}
+
---
+++ WKeyTiming/KeyTimingForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace WKeyTiming
+{
+    public partial class KeyTimingForm : Form
+    {
+
+        Stopwatch _stop_watch = new Stopwatch();
+
+        private GlobalKeyboardHook _globalKeyboardHook;
+
+        int _last_key_press = -1;
+        TimeSpan _last_timespan = new TimeSpan(0);
+
+        delegate void setTextCallback(string txt);
+
+        KeysConverter _key_converter = new KeysConverter();
+
+        public KeyTimingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyTimingForm_Load(object sender, EventArgs e)
+        {
+            SetupKeyboardHooks();
+
+            _stop_watch.Start();
+        }
+
+        public void SetupKeyboardHooks()
+        {
+            _globalKeyboardHook = new GlobalKeyboardHook();
+            _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+        }
+
+        private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
+            {
+                //e.Handled = true;
+                string msg;
+                if (e.KeyboardData.VirtualCode != _last_key_press)
+                {
+                    _last_key_press = e.KeyboardData.VirtualCode;
+
+                    msg = string.Format("Key {0}\r\n", _key_converter.ConvertToString(e.KeyboardData.VirtualCode));
+                    setOutputStatusText(msg);
+                }
+
+                msg = string.Format("Time {0}\r\n", _stop_watch.Elapsed.TotalMilliseconds);
+                setOutputStatusText(msg);
+
+                _stop_watch.Restart();
+
+            }
+
+            void setOutputStatusText(string text)
+            {
+                if (Status_textBox.InvokeRequired)
+                {
+                    setTextCallback d = new setTextCallback(setOutputStatusText);
+                    this.Invoke(d, new object[] { text });
+                }
+                else
+                {
+                    Status_textBox.AppendText(text);
+                    Status_textBox.Update();
+                }
+            }
+        }
+
+    }
+}
